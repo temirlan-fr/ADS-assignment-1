a131e2e79f88380323905a38c6e6590a5d1c0579
temirlan-fr <bolattimok197227@gmail.com>
2026-03-26 19:04:23 +0500

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,24 @@ public class Main {
 
         int result = SumOfSquares(a);
         System.out.println("Sum of squares is " + result);
+
+
+//      TASK 2
+        Scanner num2 = new Scanner(System.in);
+        int N = 6;
+        int[] arr = new int[N];
+
+        System.out.println("Fill in numbers for array: ");
+        for (int i = 0; i < N; i++){
+            arr[i] = num2.nextInt();
+        }
+
+        System.out.println("What are first n you want to sum: ");
+        int n = num2.nextInt();
+
+        int result2 = SumOfFirstNelement(arr, n);
+        System.out.println("Sum of first n elements = " + result2);
+
     }
 
 //  TASK 1
@@ -20,5 +38,11 @@ public class Main {
     }
 
 
-
+    //  TASK 2
+    public static int SumOfFirstNelement (int[] arr, int n) {
+        if (n <= 0){
+            return 0;
+        }
+        return SumOfFirstNelement(arr, n-1) + arr[n-1];
+    }
 }
