5dda959d79463a69f8ee975bdfaaf7c282544ee9
temirlan-fr <bolattimok197227@gmail.com>
2026-03-26 19:00:42 +0500

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-f
+
+//      TASK 1
+        Scanner num = new Scanner(System.in);
+        int a = num.nextInt();
+
+        int result = SumOfSquares(a);
+        System.out.println("Sum of squares is " + result);
     }
+
+//  TASK 1
+    public static int SumOfSquares(int n){
+        if (n == 0){
+            return 0;
+        }
+        return SumOfSquares(n-1) + n*n;
+    }
+
+
+
 }
